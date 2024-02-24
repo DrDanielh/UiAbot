9d0d902e0c1592c2d4c6e4a03fab7a066c81ce26
Daniel Hagen <159185830+DrDanielh@users.noreply.github.com>
2024-02-24 23:27:45 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../jl'))
 
 project = 'UiAbot Documentation'
-copyright = '2023, University of Agder'
+copyright = '2024, University of Agder'
 author = 'Daniel Hagen, Martin Mæland, Tarjei Skotterud, and Martin Dahlseng Hermansen'
 release = '0.0.1'
 
